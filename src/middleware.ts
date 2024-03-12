453c3aead0d72dd7a07becd72627ed78bbdf98a9
GuruYupp <raghavendram@yupptv.com>
2024-03-12 13:05:55 +0530

    husky 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,6 @@ export function middleware(request: NextRequest) {
   
   if (isprotectedPath) {
     if(!isLoggedin){
-       
         return NextResponse.redirect(new URL('/signin',request.url))
     }
   }
