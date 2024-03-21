3f924f33a71ec9b671ddff8ff035c41ed3812a39
GuruYupp <raghavendram@yupptv.com>
2024-03-21 14:43:13 +0530

    lint setting changes

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // The list of files to lint.
+    // "*.js": ["eslint"],
+    // The folder to check.
+    "src/**/*.tsx": ["eslint","prettier"],
+    "src/**/*.ts": ["eslint","prettier"],
+  };
