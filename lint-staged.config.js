a4e3e293033d61e40809543f9dff0ff23d2b76e3
GuruYupp <raghavendram@yupptv.com>
2024-03-22 00:28:41 +0530

    lint chnages3

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
     // The list of files to lint.
     // "*.js": ["eslint"],
     // The folder to check.
-    "src/**/*.tsx": ["eslint"],
-    "src/**/*.ts": ["eslint"],
+    "src/**/*.tsx": ["eslint","prettier --write"],
+    "src/**/*.ts": ["eslint","prettier --write"],
   };
